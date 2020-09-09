ef9714f82dbeed8414106dd434d9139734530070
prasadalle <alle.reddy@gmail.com>
2020-09-09 09:48:16 -0700

    Update main.tf
    Data Lake CI/CD Testing 1

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# Data Lake CI/CD Testing 1
 terraform {
   required_version = ">= 0.12.0"
 }
